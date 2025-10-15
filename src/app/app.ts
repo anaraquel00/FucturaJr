ad7bd22297355638f8b5c89883ce8390000bd3d4
anaraquel <anaraquel00@gmail.com>
2025-10-15 19:05:10 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Header } from "./header/header";
+import { Home } from "./home/home";
+import { Footer } from "./footer/footer";
+import { Projets } from "./projets/projets";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Header, Home, Footer, Projets],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('FucturaJr');
+  constructor() {
+
+  }
 }
