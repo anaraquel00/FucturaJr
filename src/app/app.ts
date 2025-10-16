a73867dd736056c1f9346b274eb7f4151b2f386a
anaraquel <anaraquel00@gmail.com>
2025-10-16 15:57:30 -0300

    add novo componente add images

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Header } from "./header/header";
 import { Home } from "./home/home";
 import { Footer } from "./footer/footer";
 import { Projets } from "./projets/projets";
+import { Programmers } from "./programmers/programmers";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home, Footer, Projets],
+  imports: [Header, Home, Footer, Projets, Programmers],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/programmers/programmers.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programmers',
+  imports: [],
+  templateUrl: './programmers.html',
+  styleUrl: './programmers.scss'
+})
+export class Programmers {
+
+}
